e2629312b1ea41b1c820f07e5a0085bfa7b90102
BorysFursov <borys.fursov@stryker.com>
2018-09-12 16:03:25 +0300

    initial

---
+++ InventoryDALDisconnectedGUI/MainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace InventoryDALDisconnectedGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.InventoryGrid = new System.Windows.Forms.DataGridView();
+            this.btnUpdateInventory = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.InventoryGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Cursor = System.Windows.Forms.Cursors.No;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Italic);
+            this.lblInstructions.Location = new System.Drawing.Point(45, 19);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(237, 29);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "You can edit it here:";
+            // 
+            // InventoryGrid
+            // 
+            this.InventoryGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.InventoryGrid.Location = new System.Drawing.Point(50, 65);
+            this.InventoryGrid.Name = "InventoryGrid";
+            this.InventoryGrid.RowTemplate.Height = 24;
+            this.InventoryGrid.Size = new System.Drawing.Size(703, 297);
+            this.InventoryGrid.TabIndex = 1;
+            this.InventoryGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnUpdateInventory
+            // 
+            this.btnUpdateInventory.Location = new System.Drawing.Point(661, 402);
+            this.btnUpdateInventory.Name = "btnUpdateInventory";
+            this.btnUpdateInventory.Size = new System.Drawing.Size(91, 36);
+            this.btnUpdateInventory.TabIndex = 2;
+            this.btnUpdateInventory.Text = "Update";
+            this.btnUpdateInventory.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUpdateInventory);
+            this.Controls.Add(this.InventoryGrid);
+            this.Controls.Add(this.lblInstructions);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.InventoryGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.DataGridView InventoryGrid;
+        private System.Windows.Forms.Button btnUpdateInventory;
+    }
+}
+
